287f739e0efecc573d34b19d3b96342ac02e2682
Yoshii1024a <lorenzo.inglese.wtnb@gmail.com>
2025-06-11 13:24:26 +0800

    Delete Web directory

--- Web/home.js
+++
@@ -1,6 +0,0 @@
-var home_dropdown = document.getElementById("home_drop")
-
-home_dropdown.addEventListener("change", function() {
-    if (home_drop.value == "notes")
-        open("notes.html", "_self")
-})
